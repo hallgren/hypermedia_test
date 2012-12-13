cf1de226e3164a1383631660cfa11e6ba4562e3c
Morgan Hallgren <morganhallgren@Morgans-MacBook-Pro.local>
2012-12-13 12:39:27 +0100

    Added more rest like formating

--- test.rb
+++ test.rb
@@ -5,33 +5,38 @@ damage_hash = {}
 
 Cuba.define do
   on get do
-    on "new_damage" do
-      res.write "<form action='/create_damage' method='post'>"
+
+    on "damage/:guid" do |guid|  
+      res.write damage_hash[guid]
+      res.write "<a rel='create_damage' href='/damage'>Create new damage</a>"
+      res.write "<a rel='self' href='/damage/#{guid}'>skadan</a>"
+    end
+
+    on "damage" do
+      res.write "<form rel='create_damage' action='/damage' method='post'>"
       res.write "Skadenummer <input type='text' name='damage_id'>"
       res.write "Skadedatum <input type='text' name='damage_date'>"
       res.write "<input type='submit' value='Submit'>"
       res.write "</form>"
     end
 
-    on "damage/:guid" do |guid|  
-    	res.write damage_hash[guid]
-    end
+    
 
     on root do
-      res.redirect "/new_damage"
+      res.redirect "/damage"
     end
 
 
   end
 
   on post do
-  	on "create_damage" do
+  	on "damage" do
 	
-	  on param("damage_id"), param("damage_date") do |damage_id, damage_date|
-	    guid = UUID.new.generate
-	    damage_hash[guid] = {:guid => guid, :damage_id => damage_id, :damage_date => damage_date}
-	    res.write "Skada skapad med uuid #{guid}, skadeid = #{damage_id}, skadedatum = #{damage_date}"
-        res.write "<a href='/damage/#{guid}'>skadan</a>"
+	    on param("damage_id"), param("damage_date") do |damage_id, damage_date|
+	      guid = UUID.new.generate
+	      damage_hash[guid] = {:guid => guid, :damage_id => damage_id, :damage_date => damage_date}
+	      res.write "Skada skapad med uuid #{guid}, skadeid = #{damage_id}, skadedatum = #{damage_date}"
+        res.write "<a rel='damage' href='/damage/#{guid}'>skadan</a>"
       end
     end
   end
